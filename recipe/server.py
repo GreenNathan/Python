1e5b1e20a192eafdc3f86c538059d3e03160b802
Nathan Green <greennathan6292@yahoo.com>
2023-11-10 09:05:09 -0800

    recipe

---
+++ recipe/server.py
@@ -0,0 +1,12 @@
+from flask_app import app
+
+                                #"users"(controller name)
+from flask_app.controllers import user_controller, car_controller
+
+
+if __name__ == "__main__":
+        app.run(debug=True)
+
+
+
+
